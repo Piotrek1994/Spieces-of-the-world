024f5cd70b957aab750ebb768618bbeaa988981b
unknown <pprus94@gmail.com>
2023-03-20 14:54:34 +0100

    footer

--- footer.php
+++ footer.php
@@ -12,7 +12,21 @@
 ?>
 
 <footer id="colophon" class="site-footer">
-
+    <div class="footer-info">
+        <p>Kontakt</p>
+        <p>Regulamin</p>
+        <p>Polityka prywatności</p>
+        <p>Polityka plików cookie</p>
+        <p>Mapa witryny</p>
+    </div>
+    <div class="footer-apps">
+        <div class="huawei-logo"
+            style="background-image: url(<?php echo get_theme_file_uri('./assets/app_gallery.png') ?>)"></div>
+        <div class="play-logo"
+            style="background-image: url(<?php echo get_theme_file_uri('./assets/google_play.png') ?>)"></div>
+        <div class="app-store-logo"
+            style="background-image: url(<?php echo get_theme_file_uri('./assets/app_store.png') ?>)"></div>
+    </div>
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
--- single.php
+++ single.php
@@ -1,21 +1,33 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package Spices_of_the_world
- */
 
 get_header();
-?>
 
-<main id="primary" class="site-main">
-    
+while(have_posts()){
+    the_post();
+    ?>
 
 
-</main><!-- #main -->
+</div>
 
-<?php
+<div class="post-container">
+    <div class="post-container2">
+        <p>
+            <a class="" href="<?php echo site_url('/'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home
+            </a> <span class="metabox__main">Posted by
+                <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in
+                <?php echo get_the_category_list(', ') ?></span>
+        </p>
+    </div>
+
+
+
+    <div class="generic-content"><?php the_content(); ?></div>
+
+
+</div>
+
+<?php }
 
 get_footer();
+
+?>
