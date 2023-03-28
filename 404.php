bba3d2a611f1932ac29f607037e8ab44109cf7a5
Piotr Prus <pprus94@gmail.com>
2023-03-28 09:55:18 +0200

    author page

--- 404.php
+++ 404.php
@@ -14,11 +14,11 @@ get_header();
 
     <section class="error-404 not-found">
         <header class="page-header">
-            <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'spieces' ); ?></h1>
+            <h1 class="page-title"><?php esc_html_e( 'Ups! Nie można znaleźć tej strony.', 'spieces' ); ?></h1>
         </header><!-- .page-header -->
 
         <div class="page-content">
-            <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'spieces' ); ?>
+            <p><?php esc_html_e( 'Spróbuj jednego z poniższych linków lub wyszukiwania.', 'spieces' ); ?>
             </p>
 
             <?php
@@ -28,7 +28,7 @@ get_header();
 					?>
 
             <div class="widget widget_categories">
-                <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'spieces' ); ?></h2>
+                <h2 class="widget-title"><?php esc_html_e( 'Najczęsciej używane kategorie', 'spieces' ); ?></h2>
                 <ul>
                     <?php
 							wp_list_categories(
@@ -46,7 +46,7 @@ get_header();
 
             <?php
 					/* translators: %1$s: smiley */
-					$spieces_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'spieces' ), convert_smilies( ':)' ) ) . '</p>';
+					$spieces_archive_content = '<p>' . sprintf( esc_html__( 'Spróbuj przeszukać archiwum miesięczne. %1$s', 'spieces' ), convert_smilies( ':)' ) ) . '</p>';
 					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$spieces_archive_content" );
 
 					the_widget( 'WP_Widget_Tag_Cloud' );
