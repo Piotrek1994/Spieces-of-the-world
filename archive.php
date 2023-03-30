fc1805b6cf4ed4883f7ac01e97a191b30469ca59
Piotr Prus <pprus94@gmail.com>
2023-03-30 12:45:41 +0200

    pagination

--- archive.php
+++ archive.php
@@ -40,11 +40,10 @@ get_header();
 
 		endwhile;
 
-		the_posts_navigation( array(
-			'prev_text' =>  '<i class="fa-sharp fa-solid fa-circle-chevron-left fa-shake fa-2xl" style="color: #f15d09;"></i>', 'text-domain',
-			'next_text' => '<i class="fa-sharp fa-solid fa-circle-chevron-right fa-shake fa-2xl" style="color: #f15d09;"></i>', 'text-domain' ,
-			'show_page_number' => true,
-			
+		the_posts_pagination( array(
+			'prev_text' =>  '<div class="icon"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></div>', 'text-domain',
+			'next_text' => '<div class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>', 'text-domain' ,
+			'show_page_number</div>' => true,
 		)	);
 	
 	else :
